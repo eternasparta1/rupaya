e1647f813e338e49a3b528c416ed27112964c737
0xfrzfrz <0x@no.way>
2018-06-19 02:08:35 +0200

    depends - fixed native_protobuf build (.exe extension management in mingw32.mk)

--- depends/builders/mingw32.mk
+++ depends/builders/mingw32.mk
@@ -1,2 +1,3 @@
 build_mingw32_SHA256SUM = sha256sum
 build_mingw32_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
+NATIVE_EXECUTABLE_EXTENSION=.exe
